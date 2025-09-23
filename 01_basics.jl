a2c72a52750618084196b3f9930707e87da553e9
Julio Novoa <9586658+julionovoa@users.noreply.github.com>
2025-09-23 08:39:14 -0700

    minor updates

--- 01_basics.jl
+++ 01_basics.jl
@@ -5,3 +5,18 @@ println(√9)
 f(x) = x^2 + x
 
 methods(f)
+
+supertypes(typeof(5))
+
+function blowup(x; max_iter=1000)
+    for count in 1:max_iter
+        x = x^2 + x
+        if isinf(x)
+            return count
+        end
+    end
+    return max_iter+1
+end
+
+blowup(0.00001,max_iter=1_000_000)
+
